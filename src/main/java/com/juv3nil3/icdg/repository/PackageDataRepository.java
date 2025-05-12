59515f03f90772a5c51212dddb958d83a6ef86fd
Navneet <navneetmui@gmail.com>
2025-05-12 18:49:33 +0530

    making documentation builder service reactive

--- src/main/java/com/juv3nil3/icdg/repository/PackageDataRepository.java
+++ src/main/java/com/juv3nil3/icdg/repository/PackageDataRepository.java
@@ -4,10 +4,15 @@ import com.juv3nil3.icdg.domain.BranchMetadata;
 import com.juv3nil3.icdg.domain.PackageData;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Repository
 public interface PackageDataRepository extends R2dbcRepository<PackageData, Long> {
 
     Mono<PackageData> findByBranchAndPackageName(BranchMetadata branch, String packageName);
+
+    Flux<PackageData> findByBranchIdAndParentPackageIsNull(Long branchId);
 }
--- src/main/java/com/juv3nil3/icdg/service/DocumentationBuilderService.java
+++ src/main/java/com/juv3nil3/icdg/service/DocumentationBuilderService.java
@@ -1,9 +1,41 @@
 package com.juv3nil3.icdg.service;
 
+import com.juv3nil3.icdg.domain.BranchMetadata;
+import com.juv3nil3.icdg.domain.Documentation;
+import com.juv3nil3.icdg.domain.PackageData;
+import com.juv3nil3.icdg.domain.RepositoryMetadata;
+import com.juv3nil3.icdg.repository.DocumentationRepository;
+import com.juv3nil3.icdg.repository.PackageDataRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class DocumentationBuilderService {
 
+    private final DocumentationRepository documentationRepository;
+    private final PackageDataRepository packageDataRepository;
 
+    public DocumentationBuilderService(DocumentationRepository documentationRepository, PackageDataRepository packageDataRepository) {
+        this.documentationRepository = documentationRepository;
+        this.packageDataRepository = packageDataRepository;
+    }
+
+    public Mono<Documentation> buildDocumentation(RepositoryMetadata repo, BranchMetadata branchMetadata) {
+        return packageDataRepository
+                .findByBranchIdAndParentPackageIsNull(branchMetadata.getId())
+                .collectList()
+                .flatMap(topLevelPackages -> {
+                    Documentation documentation = new Documentation();
+                    documentation.setBranchMetadata(branchMetadata);
+                    documentation.setCreatedAt(LocalDateTime.now());
+                    documentation.setUpdatedAt(LocalDateTime.now());
+                    documentation.setPackages(topLevelPackages);
+
+                    return documentationRepository.save(documentation);
+                });
+    }
 }
